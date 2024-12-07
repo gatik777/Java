be85a1e7d4dcd12689fcdff50c08979cdfa8f9ae
Gatik Prasher <167497894+gatik777@users.noreply.github.com>
2024-12-07 21:33:13 +0530

    Update Main.java

--- RegularExpressions/src/Main.java
+++ RegularExpressions/src/Main.java
@@ -1,10 +1,37 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
         String helloWorld = "%s%s.".formatted("Hello", "World");
-        String helloWorld2 = String.format("Hello", "World");
+        System.out.println("Using string's formatted method: " + helloWorld);
 
-        System.out.println(helloWorld);
-        System.out.println(helloWorld2);
+        String helloWorld2 = String.format("Hello", "World");
+        System.out.println("Using Main.format : " + helloWorld2);
+
+        format("Hello, World", "\".*%s.*\"");
+
+
+
     }
+
+    private static String format(String regexp, String... args) {
+
+
+        int index = 0;
+        while (regexp.matches(".*%s.*")) {
+            regexp = regexp.replaceFirst("%s", args[index++]);
+            System.out.println(regexp);
+
+            String helloWorld3 = Main.format("%s %s", "Hello" , "World");
+            System.out.println(helloWorld3);
+        }
+
+        return regexp;
+
+    }
+
+
+
+
 }
